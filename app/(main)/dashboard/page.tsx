9a217d331b8b9c3fa3ca2a2ec2a8726af014a096
Aditya-Kayasth <adikayasth@gmail.com>
2025-10-28 00:38:19 +0530

    UI Improvements

--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -128,7 +128,7 @@ export default function DashboardPage() {
       </div>
 
       <div className="flex justify-center">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 w-screen px-20">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 w-screen px-16">
           {data.articles.map((article) => (
             <ArticleCard key={article.url} article={article} />
           ))}
